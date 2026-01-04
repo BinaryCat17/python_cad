ce8d9229312e6810daceae2e7b4adfd693abb9eb
BinaryCat17 <vv.smirnov17@ya.ru>
2026-01-04 21:07:06 +0300

    feat: finalize robust slider with 8mm hook and add straight stopper bar to visor

--- projects/tablet_holder/holder_half.py
+++ projects/tablet_holder/holder_half.py
@@ -3,7 +3,7 @@ from build123d import *
 import math
 
 def build_holder_half(params: dict, is_left: bool = True) -> Part:
-    """Строит половину корпуса с открытым снизу пазом для слайдера."""
+    """Строит половину корпуса с прямой фиксирующей планкой."""
     
     tw, th, tt = params["tablet_w"], params["tablet_h"], params["tablet_t"]
     wall = params["wall"]
@@ -11,13 +11,11 @@ def build_holder_half(params: dict, is_left: bool = True) -> Part:
     hd = params["adapter_hole_dist"]
     vd = params["visor_d"]
     
-    # Высота: планшет + 60мм (было 48)
     th_total = th + 60.0 
     hw = (tw + wall * 2) / 2
     
-    # Новые параметры для паза слайдера
     st = params.get("slider_front_t", 6.0)
-    sw = params.get("slider_f_width", 110.0)
+    sw = params.get("slider_f_width", 140.0)
     panel_t = wall + st
     total_depth = tt + panel_t + vd
     
@@ -25,63 +23,51 @@ def build_holder_half(params: dict, is_left: bool = True) -> Part:
         x_dir = -1 if is_left else 1
         align_x = Align.MAX if is_left else Align.MIN
         
-        # 1. Задняя панель (утолщенная)
+        # 1. Задняя панель
         Box(hw, th_total, panel_t, align=(align_x, Align.CENTER, Align.MIN))
         
         # 2. Боковая стенка (Ковш)
         with BuildPart(mode=Mode.ADD):
             with BuildSketch(Plane.YZ.offset(x_dir * hw)) as s:
                 with BuildLine():
-                    y_top, y_bot = th_total/2, -th_total/2
-                    p1, p2 = (y_bot, 0), (y_top, 0)
-                    p3 = (y_top, total_depth)
-                    p_bend = (y_top / 2, total_depth)
-                    p4 = (y_bot, tt + panel_t)
+                    p1, p2 = (-th_total/2, 0), (th_total/2, 0)
+                    p3 = (th_total/2, total_depth)
+                    p_bend = (th_total/4, total_depth)
+                    p4 = (-th_total/2, panel_t + tt)
                     Polyline(p1, p2, p3, p_bend, p4, close=True)
                 make_face()
-                # Мощные скругления в 2D - это 100% стабильно
-                # Скругляем верхний угол (p3) и нижний угол (p4) на 25мм
-                v_corners = s.vertices().filter_by(Axis.Z, total_depth-1, total_depth+1)
-                if v_corners:
-                    fillet(v_corners, radius=25)
             extrude(amount=-x_dir * wall)
 
         # 3. Верхний козырек (Roof)
         roof_depth = total_depth - panel_t
         with BuildPart(mode=Mode.ADD):
-            # Используем обычный Box, так как боковина со скруглением 25мм все равно задает основной профиль
             with Locations((0, th_total/2 - wall/2, panel_t + roof_depth/2)):
                 Box(hw, wall, roof_depth, align=(align_x, Align.CENTER, Align.CENTER))
-
-        # 4. Направляющие пазы
             
+            # 3.2 ФИКСИРУЮЩАЯ ПЛАНКА (Прямая, во всю ширину)
+            bar_h, bar_t = 12.0, 8.0
+            # Размещаем планку на краю планшета (Z = panel_t + tt)
+            with Locations((0, th_total/2 - wall - bar_h/2, panel_t + tt + bar_t/2)):
+                Box(hw, bar_h, bar_t, align=(align_x, Align.CENTER, Align.CENTER))
+
         # 4. Направляющие пазы
         tablet_bottom_y = th_total/2 - wall - th
-        slot_top = tablet_bottom_y
-        # Паз до самого низа панели
-        slot_bottom = -th_total/2
+        slot_top, slot_bottom = tablet_bottom_y, -th_total/2
         
-        # Широкий паз под передний блок слайдера (sw = 140)
         with Locations((0, (slot_top + slot_bottom)/2, panel_t - st/2)):
             Box(sw/2 + 1, slot_top - slot_bottom, st + 0.5, align=(align_x, Align.CENTER, Align.CENTER), mode=Mode.SUBTRACT)
-
-        # Узкий сквозной паз под шейку (фиксировано 110мм для болтов)
         with Locations((0, (slot_top + slot_bottom)/2, panel_t/2)):
             Box(55, slot_top - slot_bottom, panel_t + 2, align=(align_x, Align.CENTER, Align.CENTER), mode=Mode.SUBTRACT)
         
-        # 5. Отверстия VESA и ниши под гайки
+        # 5. Отверстия VESA
         xh = x_dir * 50.0
         for yh in [-50.0, 50.0]:
             with Locations((xh, yh, 0)):
-                # Используем Align.MIN, чтобы цилиндр шел от 0 вверх на всю толщину
                 Cylinder(radius=5.5/2, height=panel_t, align=(Align.CENTER, Align.CENTER, Align.MIN), mode=Mode.SUBTRACT)
                 with BuildSketch(Plane.XY.offset(panel_t)) as s:
-                    with Locations((xh, yh)):
-                        RegularPolygon(radius=9.5/2, side_count=6)
+                    with Locations((xh, yh)): RegularPolygon(radius=9.5/2, side_count=6)
                 extrude(amount=-4.0, mode=Mode.SUBTRACT)
 
-        # 6. ДЖОЙНТЫ
-        # slider_start теперь на дне паза (Z = wall)
         RigidJoint("slider_start", obj.part, Location((0, tablet_bottom_y, wall)))
         RigidJoint("adapter_mount", obj.part, Location((0, 0, 3)))
     
--- projects/tablet_holder/slider.py
+++ projects/tablet_holder/slider.py
@@ -2,50 +2,48 @@ from __future__ import annotations
 from build123d import *
 
 def build_slider(params: dict) -> Part:
-    """Строит монолитный слайдер с глубокой интеграцией узлов."""
-    wall = params["wall"]
-    tt = params["tablet_t"]
-    st = params.get("slider_front_t", 6.0)
-    sw = params.get("slider_f_width", 140.0) # 140мм
+    """Строит монолитный слайдер с усиленной Г-образной платформой."""
+    wall, tt = params["wall"], params["tablet_t"]
+    st, sw = params.get("slider_front_t", 6.0), params.get("slider_f_width", 140.0)
+    
+    # Планшет (18мм) начинается от Z=st (6.0)
+    tablet_front_z = st + tt
     
     with BuildPart() as obj:
-        # 1. Передний блок (Лапа + блок в пазу)
+        # 1. Основной профиль: Блок в пазу + Мощный Г-зацеп
         with BuildSketch(Plane.YZ) as s1:
             with BuildLine():
-                p1 = (0, st + 14)
-                p2 = (-6, st + 14)
-                p3 = (-6, st)
-                p4 = (-60, st)
-                p5 = (-60, 0)
-                p6 = (0, 0)
-                Polyline(p1, p2, p3, p4, p5, p6, close=True)
+                # Y - вертикаль, Z - глубина
+                Polyline(
+                    (0, 0),                   # 1. Верх-зад блока в пазу
+                    (-60, 0),                 # 2. Низ-зад блока
+                    (-60, st),                # 3. Низ-перед блока (у панели)
+                    (-10, st),                # 4. Толщина платформы снизу (10мм)
+                    (-10, tablet_front_z + 8),# 5. Вылет платформы (Z=32)
+                    (20, tablet_front_z + 8), # 6. Зацеп ВВЕРХ (высота 20мм)
+                    (20, tablet_front_z),     # 7. Загиб к экрану (толщина 8мм)
+                    (0, tablet_front_z),      # 8. Внутренний угол платформы
+                    close=True
+                )
             make_face()
         extrude(amount=sw/2, both=True)
 
-        # 2. Шейка (Neck)
+        # 2. Шейка (108мм)
         with BuildPart(mode=Mode.ADD):
             with BuildSketch(Plane.YZ) as s2:
                  Rectangle(60, wall + 4, align=(Align.MAX, Align.CENTER))
-                 fillet(s2.vertices(), radius=2)
             extrude(amount=108/2, both=True)
 
-        # 3. Задняя плита (Back Plate)
+        # 3. Задняя плита (170мм)
         with BuildPart(mode=Mode.ADD):
-            with BuildSketch(Plane.YZ) as s3:
+            with BuildSketch(Plane.YZ.offset(-wall)) as s3:
                 with BuildLine():
-                    p1 = (0, -wall)
-                    p2 = (0, -wall-6)
-                    p3 = (-65, -wall-6)
-                    p4 = (-65, -wall)
-                    Polyline(p1, p2, p3, p4, close=True)
+                    Polyline((0, 0), (0, -6), (-65, -6), (-65, 0), close=True)
                 make_face()
-            extrude(amount=160/2, both=True)
+            extrude(amount=170/2, both=True)
 
-        # 4. Отверстия под болты-анкеры
-        # Болты на x = +/- 50мм.
+        # 4. Отверстия под болты
         with BuildPart(mode=Mode.SUBTRACT):
-            # Отверстия на Y = -30 (середина шейки)
-            # Центр отверстий по Z (мировой) = -wall/2
             with Locations(Plane.XY.offset(-wall/2) * Location((0, -30, 0))):
                 for x_pos in [-50, 50]:
                     with Locations((x_pos, 0, 0)):
